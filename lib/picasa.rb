b5dcb2c8ea5b4d9bf37f3630293dc5f0cb326370
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-07-02 22:34:13 +0200

    raise argument error if google_user not specified

--- lib/picasa.rb
+++ lib/picasa.rb
@@ -5,11 +5,15 @@ module Picasa
   require 'web_albums.rb'
 
   def self.albums(options = {})
+    raise ArgumentError, "You must specify google_user" unless options[:google_user]
     web_albums = Picasa::WebAlbums.new(options[:google_user])
     web_albums.albums
   end
 
   def self.photos(options = {})
+    unless options[:google_user] and options[:album_id]
+      raise ArgumentError, "You must specify google_user and album_id"
+    end
     web_albums = Picasa::WebAlbums.new(options[:google_user])
     web_albums.photos(options[:album_id])
   end
