70d16209c9180b95aabedb9acdaf7d652a6cb327
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2011-12-01 23:55:27 +0100

    do not require rubygems in test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,7 @@
-require "rubygems"
 gem "test-unit"
 require "test/unit"
 require "fakeweb"
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require "picasa"
 
 def fixture_file(filename)
