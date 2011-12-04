1fd660c121848ed759aed906aed48814f4a4656b
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2011-12-04 19:53:21 +0100

    block all http requests by fakeweb

--- test/helper.rb
+++ test/helper.rb
@@ -3,8 +3,14 @@ require "fakeweb"
 
 require "picasa"
 
-def fixture_file(filename)
-  return "" if filename == ""
-  file_path = File.expand_path(File.dirname(__FILE__) + "/fixtures/" + filename)
-  File.read(file_path)
+class MiniTest::Unit::TestCase
+  def setup
+    FakeWeb.allow_net_connect = false
+  end
+
+  def fixture_file(filename)
+    return "" if filename == ""
+    file_path = File.expand_path(File.dirname(__FILE__) + "/fixtures/" + filename)
+    File.read(file_path)
+  end
 end
