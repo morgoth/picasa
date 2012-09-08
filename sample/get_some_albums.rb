ce9bfa7d3dcebdaca1a8ac4563ef501d97b79813
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-09-09 01:26:56 +0900

    create sample directory

---
+++ sample/get_some_albums.rb
@@ -0,0 +1,13 @@
+require "./picasa.rb"
+
+client = Picasa::Client.new(:user_id => "your_gmail_account", :password => "password")
+puts client
+
+begin
+	# get some albums.
+	albums = client.album.list.entries
+	for album in albums do puts album.title end
+	
+rescue Picasa::ForbiddenError
+	puts "user_id is not found"
+end
