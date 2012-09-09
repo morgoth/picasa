654c6a31b0bb3fcb1b16ac6eee8f6cb2424814b5
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-09-09 21:13:56 +0900

    add throw arguments (:summary, :access)

--- sample/create_new_album.rb
+++ sample/create_new_album.rb
@@ -4,7 +4,10 @@ client = Picasa::Client.new(:user_id => "your_gmail_account", :password => "pass
 
 begin
 	# create new album.
-	client.album.create(:title => "New Album")
+	client.album.create(
+		:title => "New Album",
+		:summary => "This is a new album.",
+		:access => "protected")
 	
 rescue Picasa::ForbiddenError
 	puts "user_id is not found"
