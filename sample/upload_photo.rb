b6ab86c7e1c6f515ffd21e6407ff2bd41c0487fa
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-09-09 16:32:59 +0900

    modify variable name

--- sample/upload_photo.rb
+++ sample/upload_photo.rb
@@ -8,16 +8,16 @@ photo_bin = File.binread("./test.jpg")
 begin
 	# create method needs an album's id.
 	albums = client.album.list.entries
-	test_album = nil
+	new_album = nil
 	for album in albums do
 		if album.title == "New Album" then
-			test_album = album
+			new_album = album
 			break
 		end
 	end
 	
 	photo = client.photo.create(
-		test_album.id, 
+		new_album.id, 
 		{:binary => photo_bin, 
 		:content_type => "image/jpeg", 
 		:title => "Test Photo",
