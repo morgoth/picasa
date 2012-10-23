31d64554393731ed954f762f2d31efa025e4fa66
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2012-10-23 22:27:42 +0200

    simplified proxy handling

--- lib/picasa/http.rb
+++ lib/picasa/http.rb
@@ -3,29 +3,26 @@ require "uri"
 
 module Picasa
   class HTTP
+    include HTTParty
+
     API_URL      = "https://picasaweb.google.com"
     API_AUTH_URL = "https://www.google.com"
     API_VERSION  = "2"
 
     def self.proxy
-      proxy_uri ||= URI.parse(ENV["https_proxy"] || ENV["HTTPS_PROXY"] || "")
-      { :addr => proxy_uri.host,
-        :port => proxy_uri.port,
-        :user => proxy_uri.user,
-        :pass => proxy_uri.password }
+      proxy_uri = URI.parse(ENV["https_proxy"] || ENV["HTTPS_PROXY"] || "")
+      [proxy_uri.host, proxy_uri.port, proxy_uri.user, proxy_uri.password]
     end
 
-    include HTTParty
+    base_uri   API_URL
 
-    base_uri       API_URL
+    headers    "User-Agent"      => "ruby-gem-picasa-v#{VERSION} (gzip)",
+               "GData-Version"   => API_VERSION,
+               "Content-Type"    => "application/atom+xml",
+               "Accept-Encoding" => "gzip, deflate"
 
-    headers        "User-Agent"      => "ruby-gem-picasa-v#{VERSION} (gzip)",
-                   "GData-Version"   => API_VERSION,
-                   "Content-Type"    => "application/atom+xml",
-                   "Accept-Encoding" => "gzip, deflate"
+    http_proxy *proxy
 
-    http_proxy     proxy[:addr], proxy[:port], proxy[:user], proxy[:pass]
-
-    format  :xml
+    format     :xml
   end
 end
