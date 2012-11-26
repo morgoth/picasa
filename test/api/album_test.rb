826ef7b0a733dac03b32b1a09df5a7efed4e42c2
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2012-11-26 21:55:01 +0100

    added missing tests for photo

--- test/api/album_test.rb
+++ test/api/album_test.rb
@@ -154,6 +154,92 @@ describe Picasa::API::Album do
     it "has allow_downloads" do
       assert_equal true, @album.allow_downloads
     end
+
+    describe "photo" do
+      before do
+        @photo = @album.entries.first
+      end
+
+      it "has id" do
+        assert_equal "5239556203823850002", @photo.id
+      end
+
+      it "has links" do
+        assert_equal 5, @photo.links.size
+      end
+
+      it "has media" do
+        assert_equal 3, @photo.media.thumbnails.size
+      end
+
+      it "has content type" do
+        assert_equal "image/jpeg", @photo.content.type
+      end
+
+      it "has etag" do
+        assert_equal "\"YD4qeyI.\"", @photo.etag
+      end
+
+      it "has published" do
+        assert_equal "2008-08-28T13:14:03+00:00", @photo.published.to_s
+      end
+
+      it "has updated" do
+        assert_equal "2009-06-24T05:19:50+00:00", @photo.updated.to_s
+      end
+
+      it "has title" do
+        assert_equal "lena2.jpg", @photo.title
+      end
+
+      it "has summary" do
+        assert_equal "", @photo.summary
+      end
+
+      it "has album id" do
+        assert_equal "5239555770355467953", @photo.album_id
+      end
+
+      it "has access" do
+        assert_equal "public", @photo.access
+      end
+
+      it "has width" do
+        assert_equal 1084, @photo.width
+      end
+
+      it "has height" do
+        assert_equal 2318, @photo.height
+      end
+
+      it "has size" do
+        assert_equal 570958, @photo.size
+      end
+
+      it "has checksum" do
+        assert_equal "", @photo.checksum
+      end
+
+      it "has timestamp" do
+        assert_equal "1219929243000", @photo.timestamp
+      end
+
+      it "has image version" do
+        assert_equal 67, @photo.image_version
+      end
+
+      it "has commenting enabled" do
+        assert_equal true, @photo.commenting_enabled
+      end
+
+      it "has comment count" do
+        assert_equal 0, @photo.comment_count
+      end
+
+      it "has license" do
+        assert_equal "All Rights Reserved", @photo.license
+      end
+    end
   end
 
   describe "exceptions" do
