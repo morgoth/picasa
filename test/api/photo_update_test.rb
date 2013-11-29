3e535e01ab15e7fb0f457af781755ce9e4996da7
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-11-29 13:19:19 +0100

    move photo to other album using album_id param

--- test/api/photo_update_test.rb
+++ test/api/photo_update_test.rb
@@ -5,22 +5,22 @@ describe Picasa::API::Photo do
   describe "#update" do
     it "updates photo details" do
       VCR.use_cassette("photo-update") do
-        attributes = { :title => "gem-test", :summary => "photo-update-test"}
+        attributes = {:title => "gem-test", :summary => "photo-update-test"}
         album_api = Picasa::API::Album.new(:user_id => "lwoggardner", :authorization_header => AuthHeader)
 
         album = album_api.create(attributes)
 
-        photo_api = Picasa::API::Photo.new(:user_id => "lwoggardner", :authorization_header => AuthHeader) 
+        photo_api = Picasa::API::Photo.new(:user_id => "lwoggardner", :authorization_header => AuthHeader)
 
         photo = photo_api.create(album.id,:file_path => fixture_path("lena.jpg"), :title => "Lena", :summary => "fixture photo")
 
-        photo = photo_api.update(album.id,photo.id, :title => "Lena updated", :summary => "")  
+        photo = photo_api.update(album.id,photo.id, :title => "Lena updated", :summary => "")
 
         assert_equal "Lena updated", photo.title
         assert_equal "", photo.summary
 
         new_timestamp = 1385553053
-        photo = photo_api.update(album.id,photo.id, :summary => "done", :timestamp => new_timestamp, :keywords => "one, two, three")
+        photo = photo_api.update(album.id, photo.id, :summary => "done", :timestamp => new_timestamp, :keywords => "one, two, three")
 
         assert_equal "done", photo.summary
         assert_equal "1385553053", photo.timestamp
@@ -33,14 +33,14 @@ describe Picasa::API::Photo do
     it "moves photos between albums" do
       VCR.use_cassette("photo-move") do
         album_api = Picasa::API::Album.new(:user_id => "lwoggardner", :authorization_header => AuthHeader)
-        photo_api = Picasa::API::Photo.new(:user_id => "lwoggardner", :authorization_header => AuthHeader) 
+        photo_api = Picasa::API::Photo.new(:user_id => "lwoggardner", :authorization_header => AuthHeader)
 
         source_album = album_api.create(:title => "gem-test", :summary => "move source")
         target_album = album_api.create(:title => "gem-test", :summary => "move target")
 
         photo = photo_api.create(source_album.id, :file_path => fixture_path("lena.jpg"), :title => "Lena")
-        
-        photo = photo_api.update(source_album.id, photo.id, :to_album_id => target_album.id)
+
+        photo = photo_api.update(source_album.id, photo.id, :album_id => target_album.id)
 
         assert_equal target_album.id, photo.album_id
 
@@ -54,6 +54,5 @@ describe Picasa::API::Photo do
         album_api.delete(target_album.id)
       end
     end
-
   end
 end
